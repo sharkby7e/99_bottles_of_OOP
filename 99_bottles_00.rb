ae79097bd9e1a9cc2f8b4aa6e54858f95b426042
sharkbyte <sgquin@gmail.com>
2022-03-17 01:56:44 -0700

    edited README and commented on first attempt

---
+++ 99_bottles_00.rb
@@ -0,0 +1,18 @@
+# 3/16 
+# first attempt at the problem lead to an infinite loop, so i added the base case 
+# also had to use ARGV to get initial number from running the script
+
+
+def bottles(num)
+  # base case (num)ber of beers left, returns when 0
+  if num<1 
+    puts "ﱘ  No more bottles of beer on the wall :("
+    return
+  end
+  puts "ﱘ  #{num} bottles of beer on the wall" 
+  puts "ﱘ  #{num} bottles of beer"
+  puts "ﱘ  Take one down, pass it around"
+  bottles(num-1)
+end
+
+bottles(ARGV[0].to_i)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 99_bottles_of_OOP
+
+Studying OOP by working on the 99 bottles problem
+
+https://sandimetz.com/99bottles
